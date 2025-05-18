599c80dcf07d83a023e6e8455e8a53701065bf32
Ayman Yassien <yaymn9463aym@gmail.com>
2025-05-18 22:21:58 +0300

    Fix Bugs

--- Rased.Infrastructure/Repositoryies/ExpenseRepository/IExpensesRepository.cs
+++ Rased.Infrastructure/Repositoryies/ExpenseRepository/IExpensesRepository.cs
@@ -1,5 +1,6 @@
 using System.Linq.Expressions;
-using Rased.Business.Dtos;
+using Rased.Business;
+
 using Rased.Infrastructure;
 using Rased.Infrastructure.Repositoryies.Base;
 
@@ -19,12 +20,12 @@ public interface IExpensesRepository : IRepository_Test<Expense, int>
     Task<decimal> CalculateTotalExpensesAmountForLastMonthAsync(int walletId, bool isShared = false, Expression<Func<Expense, bool>>[]? filter = null);
     Task<decimal> CalculateTotalExpensesAmountForLastYearAsync(int walletId, bool isShared = false, Expression<Func<Expense, bool>>[]? filter = null);
     Task<decimal> CalculateTotalExpensesAmountForSpecificPeriodAsync(int walletId, DateTime startDateTime, DateTime endDateTime, Expression<Func<Expense, bool>>[]? filter = null, bool isShared = false);
-    public Task<List<MonthlyExpenseSummary>> SumExpensesByMonthAsync(int walletId, DateTime startDateTime, DateTime endDateTime, Expression<Func<Expense, bool>>[]? filter = null, bool isShared = false);
+    // public Task<List<MonthlyExpenseSummary>> SumExpensesByMonthAsync(int walletId, DateTime startDateTime, DateTime endDateTime, Expression<Func<Expense, bool>>[]? filter = null, bool isShared = false);
     Task<decimal> CalculateAverageDailyExpenseAmountForLastWeek(int walletId, bool isShared);
     // Task<decimal> CalculateAverageExpenseAmountForLastWeek(int walletId, bool isShared);
     Task<int> CountExpensesAsync(int walletId, bool isShared);
     Task<int> GetNumberOfRelatedBudgets(int walletId, bool isShared);
     Task<decimal> GetTotalAmountOfRelatedBudgets(int walletId, bool isShared);
-    Task<List<MonthlyExpenseSummary>> SumExpensesByYearAsync(int walletId, bool isShared);
+    // Task<List<MonthlyExpenseSummary>> SumExpensesByYearAsync(int walletId, bool isShared);
     
 }
