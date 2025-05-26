910fb53f4c3b568da70546438ce653ceeeecc870
BnAdel <mohamed.adel.elsayed.fci@gmail.com>
2025-05-26 16:35:19 +0300

    GetUserData, UpdateUserData, integrate them!

---
+++ Rased.Business/Dtos/Auths/UpdateUserDto.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Rased.Business.Dtos.Auths
+{
+    public class UpdateUserDto
+    {
+        [Required]
+        public string FullName { get; set; } = null!;
+        [Required]
+        public string UserName { get; set; } = null!;
+        public string? Address { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? ProfilePic { get; set; }
+    }
+}
